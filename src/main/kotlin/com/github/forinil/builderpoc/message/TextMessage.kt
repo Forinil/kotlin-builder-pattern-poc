86ce880b33afe54e9903ce188e0ff9a659da1bd5
Konrad Botor <kbotor@gmail.com>
2018-04-12 22:38:20 +0200

    :wrench: Allowed to chain builder methods in any order

--- src/main/kotlin/com/github/forinil/builderpoc/message/TextMessage.kt
+++ src/main/kotlin/com/github/forinil/builderpoc/message/TextMessage.kt
@@ -3,20 +3,27 @@ package com.github.forinil.builderpoc.message
 import com.github.forinil.builderpoc.message.abstract.Message
 
 open class TextMessage protected constructor(protected val message: String, type: Type): Message(type) {
-    open class Builder: Message.Builder<Builder, TextMessage>() {
+    open class Builder<B: Builder<B>>: Message.Builder<B>() {
         protected var message = ""
 
-        override fun self(): Builder {
-            return this
+        @Suppress("UNCHECKED_CAST")
+        override fun self(): B {
+            return this as B
         }
 
         override fun build(): TextMessage {
             return TextMessage(message, type)
         }
 
-        fun message(value: String): Builder {
+        fun message(value: String): B {
             message = value
             return self()
+        }
+    }
+
+    class ConcreteBuilder private constructor(): Builder<ConcreteBuilder>() {
+        companion object {
+            fun new() =  ConcreteBuilder()
         }
     }
 
@@ -25,8 +32,8 @@ open class TextMessage protected constructor(protected val message: String, type
     }
 
     companion object {
-        fun newBuilder(): Builder {
-            return Builder()
+        fun newBuilder(): ConcreteBuilder {
+            return ConcreteBuilder.new()
         }
     }
 }
